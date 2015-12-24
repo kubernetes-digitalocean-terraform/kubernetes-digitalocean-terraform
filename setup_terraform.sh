114907ac2b53eb0c68e51a7c0c47de821f2ac24c
Marcos Sánchez <arkanus@gmail.com>
2015-12-23 21:59:01 -0300

    Simple script to setup terraform stuff

---
+++ setup_terraform.sh
@@ -0,0 +1,13 @@
+## Setup terraform envvars
+# Usage:
+#	. ./setup_terraform.sh
+
+export TF_VAR_do_token=$(cat ./secrets/DO_TOKEN)
+export TF_VAR_pub_key="~/.ssh/id_rsa.pub"
+export TF_VAR_pvt_key="~/.ssh/id_rsa"
+
+if [[ `uname` == 'Darwin' ]]; then
+	export TF_VAR_ssh_fingerprint=$(ssh-keygen -E MD5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}' | sed 's/MD5://g')
+else
+	export TF_VAR_ssh_fingerprint=$(ssh-keygen -lf ~/.ssh/id_rsa.pub | awk '{print $2}')
+fi
