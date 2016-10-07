a0cc032e11106e23d1e1856302a9e94a8ea93e81
Stefan Codrescu <ssmmcc1@gmail.com>
2016-10-07 00:38:08 -0600

    Fix typo

--- deploy.tf
+++ deploy.tf
@@ -363,7 +363,7 @@ resource "null_resource" "deploy_microbot" {
     depends_on = ["null_resource.setup_kubectl"]
     provisioner "local-exec" {
         command = <<EOF
-            sed -e "s/\$EXT_IP1/${digitalocean_droplet.k8s_worker.0.ipv4_address}/" < 03-microbot.yaml > ./secrets/04-microbot.rendered.yaml
+            sed -e "s/\$EXT_IP1/${digitalocean_droplet.k8s_worker.0.ipv4_address}/" < 03-microbot.yaml > ./secrets/03-microbot.rendered.yaml
             until kubectl get pods 2>/dev/null; do printf '.'; sleep 5; done
             kubectl create -f ./secrets/03-microbot.rendered.yaml
 
