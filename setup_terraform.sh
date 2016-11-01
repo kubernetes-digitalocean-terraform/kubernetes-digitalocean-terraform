f3a2cf672a0be75aa8abe3b5d359e2121d3e774a
Stefan <5tefan@users.noreply.github.com>
2016-11-01 13:08:36 -0600

    Fixes #14, setopts appropriately if in a zsh shell (#17)
    * Fixes #14, setopts appropriately if in a zsh shell
    
    `setopt KSH_ARRAYS` enables 0 indexing arrays, needed for compat
    with bash as opposed to zsh native 1 indexed arrays.
    
    Also for `setopt BASH_REMATCH`: see [1]
    > If the option BASH_REMATCH is set the array BASH_REMATCH
    > is set to the substring that matched the pattern followed
    > by the substrings that matched parenthesised subexpressions
    > within the pattern.
    
    [1] http://zsh.sourceforge.net/Doc/Release/Conditional-Expressions.html
    
    * isolates zsh options scope
    
    We isolate the scope of zsh options by setting them inside
    a function with the LOCAL_OPTIONS[1] set so that KSH_ARRAYS
    and BASH_REMATCH will not conflict with any existing settings.
    
    See #17 for details.
    
    [1] http://zsh.sourceforge.net/Doc/Release/Options.html#index-LOCALOPTIONS

--- setup_terraform.sh
+++ setup_terraform.sh
@@ -5,11 +5,16 @@
 export TF_VAR_number_of_workers=3
 export TF_VAR_do_token=$(cat ./secrets/DO_TOKEN)
 
-# ssh -V prints to stderr, redirect
-ssh_ver=$(ssh -V 2>&1)
-[[ $ssh_ver =~ OpenSSH_([0-9][.][0-9]) ]] && version="${BASH_REMATCH[1]}"
+function get_ssh_version {
+    # ssh -V prints to stderr, redirect
+    ssh_ver=$(ssh -V 2>&1)
+    [[ -n $ZSH_VERSION ]] && setopt LOCAL_OPTIONS KSH_ARRAYS BASH_REMATCH
+    [[ $ssh_ver =~ OpenSSH_([0-9][.][0-9]) ]] && echo "${BASH_REMATCH[1]}"
+}
+    
+
 # if ssh version is under 6.9, use -lf, otherwise must use the -E version
-if ! awk -v ver="$version" 'BEGIN { if (ver < 6.9) exit 1; }'; then
+if ! awk -v ver="$(get_ssh_version)" 'BEGIN { if (ver < 6.9) exit 1; }'; then
     export TF_VAR_ssh_fingerprint=$(ssh-keygen -lf ~/.ssh/id_rsa.pub | awk '{print $2}')
 else
     export TF_VAR_ssh_fingerprint=$(ssh-keygen -E MD5 -lf ~/.ssh/id_rsa.pub | awk '{print $2}' | sed 's/MD5://g')
