036c7cafedf859af3f391e06d36a503f16133c6a
kevin <kevin@ki-ai.org>
2018-04-05 09:07:49 -0400

    fix network errors

--- 01-worker.sh
+++ 01-worker.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 set -o nounset -o errexit
 
-echo --node-ip=${NODE_PRIVATE_IP} >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+echo Environment=KUBELET_EXTRA_ARGS=--node-ip=${NODE_PRIVATE_IP} >> /etc/systemd/system/kubelet.service.d/10-kubeadm.conf
+
 systemctl daemon-reload
 
 eval $(cat /tmp/kubeadm_join)
