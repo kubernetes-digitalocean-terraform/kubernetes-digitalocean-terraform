1d49f7c18fecbb9d90796fb61928f0f006d4ff63
Tom George <tg82490@gmail.com>
2018-07-07 01:14:50 +0000

    weird sed issue

--- deploy.tf
+++ deploy.tf
@@ -117,7 +117,7 @@ resource "digitalocean_droplet" "k8s_master" {
     provisioner "local-exec" {
         command =<<EOF
             scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i ${var.ssh_private_key} core@${digitalocean_droplet.k8s_master.ipv4_address}:"/tmp/kubeadm_join /etc/kubernetes/admin.conf" ${path.module}/secrets/
-            sed -i '.bak' "s/${self.ipv4_address_private}/${self.ipv4_address}/" ${path.module}/secrets/admin.conf
+            sed -i.bak "s/${self.ipv4_address_private}/${self.ipv4_address}/" ${path.module}/secrets/admin.conf
 EOF
     }
 
